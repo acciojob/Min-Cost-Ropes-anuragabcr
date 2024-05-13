855d5a7e145e236b324419fa747df212c054e497
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-05-13 15:57:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
+function mincost(arr){ 
+arr.sort((a, b) => a - b);
   
+  let totalCost = 0;
+  
+  while (arr.length > 1) {
+    const min1 = arr.shift();
+    const min2 = arr.shift();
+    
+    const cost = min1 + min2;
+    
+    totalCost += cost;
+    arr.push(cost);
+    arr.sort((a, b) => a - b);
+  }
+  
+  return totalCost;
 }
 
 module.exports=mincost;
